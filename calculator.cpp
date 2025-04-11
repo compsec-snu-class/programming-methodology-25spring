ac9c8f1c6b7d3fa482a8b8cf6b2ac8cad16467d0
Jinha <james4041@snu.ac.kr>
2025-04-11 12:23:39 +0900

    1

--- calculator.cpp
+++ calculator.cpp
@@ -3,16 +3,20 @@
 
 int Calculator::add(int a, int b) {
     // TODO
+    return a + b;
 }
 
 int Calculator::sub(int a, int b) {
     // TODO
+    return a - b;
 }
 
 int Calculator::mul(int a, int b) {
     // TODO
+    return a * b;
 }
 
 int Calculator::div(int a, int b) {
     // TODO
+    return a / b;
 }
