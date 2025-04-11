fb9859c95f879403cce5cf179df1abee8307d586
junyoung <goodjy2005@snu.ac.kr>
2025-04-11 13:02:58 +0900

    upload

--- calculator.cpp
+++ calculator.cpp
@@ -1,18 +1,44 @@
 #include "calculator.h"
 #include <stdexcept>
-
+#include <limits>
+#include <iostream>
 int Calculator::add(int a, int b) {
-    // TODO
+    if ((b>0)&&(a>std::numeric_limits<int>::max()-b)){
+        throw std::overflow_error("overflow");
+    }
+    if ((b<0)&&(a<std::numeric_limits<int>::min()-b)){
+        throw std::overflow_error("overflow");
+    }
+    return a+b;
 }
 
 int Calculator::sub(int a, int b) {
-    // TODO
+    if ((b>0)&&(a<std::numeric_limits<int>::min()+b)){
+        throw std::overflow_error("overflow");
+    }
+    if ((b<0)&&(a>std::numeric_limits<int>::max()+b)){
+        throw std::overflow_error("overflow");
+    }
+
+    return a-b;
 }
 
 int Calculator::mul(int a, int b) {
-    // TODO
+    if ((a>0)&&(b>std::numeric_limits<int>::max()/a)){
+        throw std::overflow_error("overflow");
+    }
+    if ((a<0)&&(b>std::numeric_limits<int>::max()/a)){
+        throw std::overflow_error("overflow");
+    }
+    return a*b;
 }
 
 int Calculator::div(int a, int b) {
-    // TODO
+    if (b == 0) {
+        throw std::invalid_argument("division by zero");
+    }
+    if (a == std::numeric_limits<int>::min() && b == -1) {
+        throw std::overflow_error("overflow");
+    }
+    return a / b;
 }
