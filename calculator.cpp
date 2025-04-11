af901773cb353e8f35772a119467dcc65e235359
woojin <eugene5759@snu.ac.kr>
2025-04-11 20:14:46 +0900

    TDD Calculator 기능 구현 완료

--- calculator.cpp
+++ calculator.cpp
@@ -3,16 +3,41 @@
 
 int Calculator::add(int a, int b) {
     // TODO
+    long long result = (long long) a + (long long) b;
+    if (result > INT_MAX || result < INT_MIN) {
+        throw std::overflow_error("overflow/underflow");
+    }
+    return static_cast<int>(result);
 }
 
 int Calculator::sub(int a, int b) {
     // TODO
+    long long result = (long long) a - (long long) b;
+    if (result > INT_MAX || result < INT_MIN) {
+        throw std::overflow_error("overflow/underflow");
+    }
+    return static_cast<int>(result);
 }
 
 int Calculator::mul(int a, int b) {
     // TODO
+    long long result = (long long) a * (long long) b;
+    if (result > INT_MAX || result < INT_MIN) {
+        throw std::overflow_error("overflow/underflow");
+    }
+    return static_cast<int>(result);
 }
 
 int Calculator::div(int a, int b) {
     // TODO
+    if (b == 0) {
+        throw std::invalid_argument("Division by zero");
+    }
+
+    if (a == INT_MIN && b == -1) {
+        throw std::overflow_error("Division overflow");
+    }
+
+    return a / b;
+
 }
